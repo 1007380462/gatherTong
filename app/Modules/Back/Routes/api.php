d5c68d30c6e371aa893edb1b8092bd89413d3246
wumengrong <1007380462@qq.com>
2016-09-23 01:49:04 -0700

    add User activity organizaiton auth Module

---
+++ app/Modules/Back/Routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your module. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/back', function (Request $request) {
+    // return $request->back();
+})->middleware('auth:api');
---
+++ app/Modules/Front/Providers/ModuleServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Front\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'front');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'front');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
